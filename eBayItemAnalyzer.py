50871724c4d799e03f58967ea580c12a3d4224cb
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-06-30 16:46:47 -0400

    Added Support for Live item analysis

--- eBayItemAnalyzer.py
+++ eBayItemAnalyzer.py
@@ -28,64 +28,110 @@ def printMinMax(nameList,priceList):
             minName = nameList[index]
 
     avrgPrice = avrgPrice / len(nameList)
-    
+
     print()
     print("Average Price")
     print("=============")
-    print(avrgPrice)
+    print("$"+str(avrgPrice))
     print()
     print("Maximum")
     print("=======")
-    print(maxName + " " + str(maxPrice))
+    print(maxName + " Sold For: $" + str(maxPrice))
     print()
     print("Minimum")
     print("=======")
-    print(minName + " " + str(minPrice))
+    print(minName + " Sold For: $" + str(minPrice))
 
 # Entry point
+mainMenuLoop = True
+while(mainMenuLoop):
+    print()
+    print("=========")
+    print("Main Menu")
+    print("=========")
+    print("1. Analyze Sold Listings")
+    print("2. Analyze Live Listings")
+    choice = input("Enter Selection: ")
+    print()
+    if choice == '1' or choice == '2':
+        mainMenuLoop = False
+
+
 print("What Item would you like to gather info about?")
 itemName = input("Item:  ")
 print()
 
 # Driver Set up
 chromeOptions = Options()
-chromeOptions.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+# chromeOptions.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 chrome_driver = r"C:\Users\7stre\Desktop\Sean\Programming\chromedriver" # Must be the path conatining chromdrive.exe
 driver = webdriver.Chrome(chrome_driver, options=chromeOptions)
 driver.implicitly_wait(10) # the driver will wait up to 10 seconds for each element to apppear on the page before throwing an error
 
-ebayAdvancedSearchLink = "https://www.ebay.com/sch/ebayadvsearch?mkevt=1&mkcid=1&mkrid=711-53200-19255-0&campid=5337590774&customid=&toolid=10001"
-print('https://duckduckgo.com')
-driver.get(ebayAdvancedSearchLink)
+if choice == '1':
+    ebayAdvancedSearchLink = "https://www.ebay.com/sch/ebayadvsearch?mkevt=1&mkcid=1&mkrid=711-53200-19255-0&campid=5337590774&customid=&toolid=10001"
+    driver.get(ebayAdvancedSearchLink)
 
-elem = driver.find_element_by_name("_nkw")
-elem.send_keys(itemName)
+    elem = driver.find_element_by_name("_nkw")
+    elem.send_keys(itemName)
 
-elem = driver.find_element_by_name("LH_Sold")
-elem.send_keys(Keys.SPACE)
+    elem = driver.find_element_by_name("LH_Sold")
+    elem.send_keys(Keys.SPACE)
 
-elem = driver.find_element_by_class_name("btn-prim")
-elem.send_keys(Keys.RETURN)
+    elem = driver.find_element_by_class_name("btn-prim")
+    elem.send_keys(Keys.RETURN)
 
-soldListingNames = driver.find_elements_by_class_name("vip")
-soldListingPrices = driver.find_elements_by_class_name("bidsold")
+    soldListingNames = driver.find_elements_by_class_name("vip")
+    soldListingPrices = driver.find_elements_by_class_name("bidsold")
 
-nameList = []
-priceList = []
+    nameList = []
+    priceList = []
 
-for index in range(len(soldListingNames)):
-    priceString = soldListingPrices[index].get_attribute("innerHTML") # Inner html of price might not only contain the price
-    price = priceString.split("$")[1] # The price is split after the $
-    tempPrice = ""
+    for index in range(len(soldListingNames)):
+        priceString = soldListingPrices[index].get_attribute("innerHTML") # Inner html of price might not only contain the price
+        price = priceString.split("$")[1] # The price is split after the $
+        tempPrice = ""
 
-    for char in price:
-        if isDollarVal(char):
-            tempPrice += char
+        for char in price:
+            if isDollarVal(char):
+                tempPrice += char
 
-    nameList.append(soldListingNames[index].get_attribute("innerHTML")) # Add the title to the nameList
-    priceList.append(float(tempPrice)) # Add the price to the price list
+        nameList.append(soldListingNames[index].get_attribute("innerHTML")) # Add the title to the nameList
+        priceList.append(float(tempPrice)) # Add the price to the price list
 
-printMinMax(nameList,priceList)
+    printMinMax(nameList,priceList)
+
+if choice == '2':
+    ebayAdvancedSearchLink = "https://www.ebay.com/sch/ebayadvsearch?mkevt=1&mkcid=1&mkrid=711-53200-19255-0&campid=5337590774&customid=&toolid=10001"
+    driver.get(ebayAdvancedSearchLink)
+
+    elem = driver.find_element_by_name("_nkw")
+    elem.send_keys(itemName)
+
+    elem = driver.find_element_by_class_name("btn-prim")
+    elem.send_keys(Keys.RETURN)
+
+    currListingNames = driver.find_elements_by_class_name("vip")
+    parentElement = driver.find_element_by_class_name("prc")
+    currListingPrices = parentElement.find_elements_by_tag_name("span")
+
+    nameList = []
+    priceList = []
+
+    for index in range(len(currListingPrices)):
+        priceString = currListingPrices[index].get_attribute("innerHTML") # Inner html of price might not only contain the price
+        print(currListingPrices[index].get_attribute("innerHTML"))
+        price = priceString.split("$")[1] # The price is split after the $
+        tempPrice = ""
+
+        for char in price:
+            if isDollarVal(char):
+                tempPrice += char
+
+        nameList.append(currListingNames[index].get_attribute("innerHTML")) # Add the title to the nameList
+        priceList.append(float(tempPrice)) # Add the price to the price list
+
+    printMinMax(nameList,priceList)
 
 """
 for index in range(len(nameList)):
